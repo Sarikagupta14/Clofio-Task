ad65cf761f95cc295134770d923877cfd6eec66a
Sarika Gupta <121433831+Sarikagupta14@users.noreply.github.com>
2023-07-12 22:17:02 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import { Box } from '@chakra-ui/react';
+import Page from './Components/Page';
+
+function App() {
+  return (
+    <Box bgColor={'white'} h={'100vh'}>
+     <Page/>
+    </Box>
+  );
+}
+
+export default App;
